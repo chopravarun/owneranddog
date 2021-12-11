f8892b0e2c31608d8ca461d14b41c7575ef93732
chopravarun <opt4varun@gmail.com>
2021-12-11 08:53:07 -0800

    first init

---
+++ src/main/java/com/richassest/ownerAndDog/OwnerAndDogApplication.java
@@ -0,0 +1,13 @@
+package com.richassest.ownerAndDog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OwnerAndDogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OwnerAndDogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/richassest/ownerAndDog/OwnerAndDogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.richassest.ownerAndDog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OwnerAndDogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
